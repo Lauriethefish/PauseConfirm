af088e90421d70e3fcf07a2912181dd223ce5533
Lauriethefish <44222319+Lauriethefish@users.noreply.github.com>
2020-12-21 10:30:14 +0000

    Version 0.2.0

--- buildBMBF.ps1
+++ buildBMBF.ps1
@@ -2,5 +2,5 @@
 & $PSScriptRoot/build.ps1
 
 if ($?) {
-    Compress-Archive -Path "./libs/arm64-v8a/libpause-confirm.so", "./libs/arm64-v8a/libcustom-types.so", "./libs/arm64-v8a/libquestui.so", "./bmbfmod.json" -DestinationPath "./pause-confirm_v0.1.0.zip" -Update
+    Compress-Archive -Path "./libs/arm64-v8a/libpause-confirm.so", "./libs/arm64-v8a/libcustom-types.so", "./libs/arm64-v8a/libquestui.so", "./bmbfmod.json" -DestinationPath "./pause-confirm_v0.2.0.zip" -Update
 }
